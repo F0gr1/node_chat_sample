92fc4baf920efc352de0594b7d11dc5ec787dd8a
Gami <19aj009@ms.dendai.ac.jp>
2020-05-30 09:56:40 +0900

    commit now

---
+++ app.js
@@ -0,0 +1,32 @@
+
+var express = require('express');//expressを使用
+var app = express();
+var http = require('http').Server(app);
+
+const path = require('path');
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 4649;
+
+app.get('/' , function(req, res){
+    res.sendFile(__dirname+'/views/landing.html');//chat.htmlへ移動
+});
+
+
+
+io.on('connection',function(socket){
+    console.log("connections"); 
+    // メッセージ送信処理
+    socket.on('chat message', function(msg){
+    //io.emit('chat message', msg);
+    io.emit('chat message', {
+        message: msg
+    });
+
+  });
+});
+
+http.listen(PORT, function(){
+    console.log('server listening. Port(v)/:' + PORT);//PORT番ポートに接続
+});
+
+app.use(express.static(path.join(__dirname, 'views')));//chat_roomディレクトリを公開);
